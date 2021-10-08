05018f4c8a5fccc18df1040c3b70fa0bbb87028d
Ckyiu <38868705+UnsignedArduino@users.noreply.github.com>
2021-10-08 23:09:57 +0000

    Make physics for attraction

--- sketch.js
+++ sketch.js
@@ -1,10 +1,20 @@
 const width = 1280;
 const height = 720;
 
+let att;
+let player;
+
 function setup() {
   createCanvas(width, height);
+  player = new Player(300, 300, 5);
+  att = new Attractor(width / 2, height / 2, 50);
 }
 
 function draw() {
     background(56);
+    att.attract(player);
+    player.update()
+
+    att.show()
+    player.show()
 }

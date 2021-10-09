43424e682f2e8c732a2be54bf05e9cd934fa6a15
Ckyiu <38868705+UnsignedArduino@users.noreply.github.com>
2021-10-09 00:36:07 +0000

    Make stars twinkle better

--- sketch.js
+++ sketch.js
@@ -3,18 +3,52 @@ const height = 720;
 const fps = 60;
 
 let starBackground;
+let starPoints = [];
+let starOriginalSizes = [];
+let starSizes = [];
 
 function makeBackground() {
   let graphics = createGraphics(width, height);
   let starCount = random(50, 100);
+  startPoints = [];
   for (let c = 0; c < starCount; c++) {
-    let x = random(0, width);
-    let y = random(0, height);
-    graphics.fill(255, 255, 255);
-    graphics.noStroke();
-    graphics.circle(x, y, 1);
+    starPoints.push(createVector(random(0, width), random(0, height)));
+    let size = random(1, 3);
+    starOriginalSizes.push(size);
+    starSizes.push(size);
   }
   return graphics;
+}
+
+function drawBackground() {
+  starBackground.background(0);
+  starBackground.fill(255);
+  starBackground.noStroke();
+  for (let index in starPoints) {
+    if (random(0.5) < 0.001) {
+      starSizes[index] = max(starOriginalSizes[index] - 1, 0);
+      setTimeout(() => {
+        starSizes[index] = max(starOriginalSizes[index] - 1, 0);
+      }, 100);
+      setTimeout(() => {
+        starSizes[index] = max(starOriginalSizes[index] - 1, 0);
+      }, 200);
+      setTimeout(() => {
+        starSizes[index] = max(starOriginalSizes[index] - 1, 0);
+      }, 300);
+      setTimeout(() => {
+        starSizes[index] = min(starOriginalSizes[index] + 1, starOriginalSizes[index]);
+      }, 800);
+      setTimeout(() => {
+        starSizes[index] = min(starOriginalSizes[index] + 1, starOriginalSizes[index]);
+      }, 900);
+      setTimeout(() => {
+        starSizes[index] = min(starOriginalSizes[index] + 1, starOriginalSizes[index]);
+      }, 1000);
+    }
+    let vector = starPoints[index];
+    starBackground.circle(vector.x, vector.y, starSizes[index]);
+  }
 }
 
 function setup() {
@@ -26,7 +60,8 @@ function setup() {
 }
 
 function draw() {
-  background(32);
+  background(0);
+  drawBackground();
   image(starBackground, 0, 0);
   updateGame();
 }
